7c95d516baa672a884b645583451b37623bfb27d
pankajw-cuelogic <pankaj.wakchaure@cuelogic.com>
2017-06-15 11:36:38 +0530

    initial commit

---
+++ test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9424c08840f936e314266c4c8bd72265efb7b2a01605d08b715c82d794991e54462cf1c89492bd48f1c7c022ca9c548313e54c0e8734cdcb34aae5289b3e0193'
